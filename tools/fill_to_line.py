1e483e395391d50d9bd2d21cee4beecc7589fb1c
Quentin Boswank <qubos@outlook.de>
2026-06-22 11:25:52 +0000

    Deduplicate symbol registration
    Simplifies maintanance
    
    `File_Flush` was allready missing in one place

--- tools/fill_to_line.py
+++ tools/fill_to_line.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import sys
 
-file_name = "sdk/os/functions/nolto/impl.cpp"
+file_name = "sdk/os/functions/nolto/symbols.def"
 
 input_line = int(sys.argv[1], base=0)
 
@@ -9,9 +9,14 @@ if input_line < 0xBEEF0001:
     print("Invalid input line. Must be greater than 0xBEEF0001")
     sys.exit(1)
     
-if input_line < 0xDEAD0001:
+if input_line < 0xC0FFE001:
     line = (input_line - 0xBEEF0001) // 2
     print(f"libc source: impl.c:{line}")
+    sys.exit(0)
+
+if input_line < 0xDEAD0001:
+    line = (input_line - 0xC0FFE001) // 2
+    print(f"gint source: syscalls.S:{line}")
     sys.exit(0)
 
 line = (input_line - 0xDEAD0001) // 2
